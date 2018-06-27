64f3fe974f2f8b72c8e4f63211bdfd8cfaebd52e
Paul Nechifor <paul@nechifor.net>
2018-06-27 20:39:56 +0100

    Move to GitLab.

--- src/main/java/net/nechifor/commentstream/stream/StreamMessage.java
+++
@@ -1,9 +0,0 @@
-package net.nechifor.commentstream.stream;
-
-public abstract class StreamMessage {
-    public final String type;
-    
-    public StreamMessage(String type) {
-        this.type = type;
-    }
-}
